47f1c58560e0cd41c22364069aa55b01f3cfe719
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2016-06-02 08:55:11 +1000

    Make view error and success logic a bit more readable

--- app/Views/Pages/Account.php
+++ app/Views/Pages/Account.php
@@ -1,26 +1,24 @@
+<?php if (isset($_SESSION['error']) || isset($_SESSION['success'])): ?>
 <div class="row align-center">
 	<?php if (isset($_SESSION['error'])): ?>
 	<div class="medium-6 text-center">
 		<div class="alert callout">
-			<?php
-			echo $_SESSION['error'];
-			unset($_SESSION['error']);
-			?>
+			<?php echo $_SESSION['error']; ?>
+			<?php unset($_SESSION['error']); ?>
 		</div>
 	</div>
-	<?php endif ?>
+	<?php endif; ?>
 
 	<?php if (isset($_SESSION['success'])): ?>
 	<div class="medium-6 text-center">
 		<div class="success callout">
-			<?php
-			echo $_SESSION['success'];
-			unset($_SESSION['success']);
-			?>
+			<?php echo $_SESSION['success']; ?>
+			<?php unset($_SESSION['success']); ?>
 		</div>
 	</div>
-	<?php endif ?>
+	<?php endif; ?>
 </div>
+<?php endif; ?>
 
 <div class="row">&nbsp;</div>
 
--- app/Views/Pages/Home.php
+++ app/Views/Pages/Home.php
@@ -1,3 +1,25 @@
+<?php if (isset($_SESSION['error']) || isset($_SESSION['success'])): ?>
+<div class="row align-center">
+	<?php if (isset($_SESSION['error'])): ?>
+	<div class="medium-6 text-center">
+		<div class="alert callout">
+			<?php echo $_SESSION['error']; ?>
+			<?php unset($_SESSION['error']); ?>
+		</div>
+	</div>
+	<?php endif; ?>
+
+	<?php if (isset($_SESSION['success'])): ?>
+	<div class="medium-6 text-center">
+		<div class="success callout">
+			<?php echo $_SESSION['success']; ?>
+			<?php unset($_SESSION['success']); ?>
+		</div>
+	</div>
+	<?php endif; ?>
+</div>
+<?php endif; ?>
+
 <div class="row">
 	<div class="small-12 columns">
 		<h3>Example Site Index.php</h3>
