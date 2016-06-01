a3057ed17fe657efc8526619e494578eb79eff7a
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2016-06-01 16:37:50 +1000

    move script into head of document

--- app/Views/Header.php
+++ app/Views/Header.php
@@ -5,5 +5,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 	<title>User Control Skeleton</title>
 	<link rel="stylesheet" href="assets/css/app.css" />
+	<script src="assets/js/app.js"></script>
 </head>
 <body>
