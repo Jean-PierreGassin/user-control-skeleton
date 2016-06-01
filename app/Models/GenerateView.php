69a4d0eaa8acda8ec33a6e484ea3260e14ec137d
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2016-06-02 08:58:54 +1000

    Indentation

--- app/Models/GenerateView.php
+++ app/Models/GenerateView.php
@@ -12,61 +12,61 @@ class GenerateView
 	{
 		switch($view) {
 			case('/'):
-			$this->view = '../app/Views/Pages/Home.php';
+				$this->view = '../app/Views/Pages/Home.php';
 
-			return $this;
+				return $this;
 
 			case('/UserNavBar'):
-			$this->view = '../app/Views/Navigation/UserNavBar.php';
+				$this->view = '../app/Views/Navigation/UserNavBar.php';
 
-			return $this;
+				return $this;
 
 			case('/GuestNavBar'):
-			$this->view = '../app/Views/Navigation/GuestNavBar.php';
+				$this->view = '../app/Views/Navigation/GuestNavBar.php';
 
-			return $this;
+				return $this;
 
 			case('/AdminNavBar'):
-			$this->view = '../app/Views/Navigation/AdminNavBar.php';
+				$this->view = '../app/Views/Navigation/AdminNavBar.php';
 
-			return $this;
+				return $this;
 
 			case('/Register'):
-			$this->view = '../app/Views/Pages/Register.php';
+				$this->view = '../app/Views/Pages/Register.php';
 
-			return $this;
+				return $this;
 
 			case('/controlPanel'):
-			$this->view = '../app/Views/Pages/ControlPanel.php';
+				$this->view = '../app/Views/Pages/ControlPanel.php';
 
-			return $this;
+				return $this;
 
 			case('/Account'):
-			$this->view = '../app/Views/Pages/Account.php';
-			$this->message = $message;
+				$this->view = '../app/Views/Pages/Account.php';
+				$this->message = $message;
 
-			return $this;
+				return $this;
 
 			case('UserTable'):
-			$this->view = '../app/Views/Admin/UserTable.php';
-			$this->message = $message;
+				$this->view = '../app/Views/Admin/UserTable.php';
+				$this->message = $message;
 
-			return $this;
+				return $this;
 
 			case('error'):
-			$_SESSION['error'] = $message;
+				$_SESSION['error'] = $message;
 
-			return;
+				return;
 
 			case('success'):
-			$_SESSION['success'] = $message;
+				$_SESSION['success'] = $message;
 
-			return;
+				return;
 
 			default:
-			$this->view = '../app/Views/Pages/Home.php';
+				$this->view = '../app/Views/Pages/Home.php';
 
-			return $this;
+				return $this;
 		}
 	}
 
