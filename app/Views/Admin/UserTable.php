3a3ad97f934998e8d7afeff7b53f50a8bd0db34d
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2016-06-03 10:35:22 +1000

    Update styling to accommodate for newly implemented columns

--- app/Views/Admin/UserTable.php
+++ app/Views/Admin/UserTable.php
@@ -1,19 +1,19 @@
 <?php if (isset($message)): ?>
 <div class="row align-center">
 	<div class="small-12 columns">
-		<table class="hover stack" style="table-layout: fixed">
+		<table class="hover stack" style="table-layout: fixed;">
 			<thead>
 				<tr>
 					<?php foreach ($message[0] as $key => $value): ?>
-						<th><?php echo $key ?></th>
+						<th style="border: 1px solid #f0f0f0; padding: 0; text-align: center;"><?php echo $key ?></th>
 					<?php endforeach ?>
 				</tr>
 			</thead>
 			<tbody>
 				<?php foreach ($message as $users): ?>
 					<tr>
-						<?php foreach ($users as $user): ?>
-							<td class="table-scroll"><?php echo $user ?></td>
+						<?php foreach ($users as $cell): ?>
+							<td class="table-scroll" style="white-space: nowrap;"><?php echo $cell ?></td>
 						<?php endforeach ?>
 					</tr>
 				<?php endforeach ?>
