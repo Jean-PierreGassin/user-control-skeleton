f1738103f3887ad1ad7df03ae18b09f4aba12fd8
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2016-06-03 10:18:52 +1000

    Update schema to accommodate for deleted users and timestamps

--- schema.sql
+++ schema.sql
@@ -1,8 +1,14 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE DATABASE user_control_skeleton;
+
+CREATE TABLE IF NOT EXISTS user_control_skeleton.users (
     id INT(6) AUTO_INCREMENT PRIMARY KEY,
     user VARCHAR(30) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
     firstname VARCHAR(30) NOT NULL,
     lastname VARCHAR(30) NOT NULL,
-    usergroup INT(2) NOT NULL
-);
+    usergroup INT(1) DEFAULT 1,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at TIMESTAMP NULL,
+		deleted_at TIMESTAMP NULL,
+		deleted INT(1) DEFAULT 0
+) DEFAULT CHARSET=utf8;
