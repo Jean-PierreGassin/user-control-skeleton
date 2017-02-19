20b3e63e6d03a82a9636880fce263e168bf07eb7
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2017-02-19 21:41:32 +1000

    Merge UserTable into ControlPanel render

--- app/Views/Pages/ControlPanel.php
+++ app/Views/Pages/ControlPanel.php
@@ -1,3 +1,15 @@
+<div class="row align-center">
+	<div class="small-12 columns">
+		<form method="POST">
+			<div class="input-group">
+				<span class="input-group-label">User:</span>
+				<input class="input-group-field" type="text" name="search_field">
+				<input type="submit" class="input-group-button button" value="Search">
+			</div>
+		</form>
+	</div>
+</div>
+
 <?php if (isset($message)): ?>
 <div class="row align-center">
 	<div class="small-12 columns">
