b6dcb0e5edfd14537dab9d764cf08ab849e65fc8
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2017-02-19 23:29:18 +1000

    Move default_timezone to bootstrap

--- app/config/app.php
+++ app/config/app.php
@@ -4,8 +4,6 @@
  * This file is used to configure generic application variables
  */
 return [
-    'timezone' => date_default_timezone_set('Australia/Brisbane'),
-
     'routes' => [
         '/' => [
                 'controller' => 'UserControlSkeleton\Controllers\AuthController',
--- public/index.php
+++ public/index.php
@@ -6,8 +6,8 @@ use Dotenv\Dotenv;
 use UserControlSkeleton\Routes;
 
 require_once __DIR__."/../vendor/autoload.php";
-require __DIR__."/../app/config/app.php";
 
+date_default_timezone_set('Australia/Brisbane');
 session_start();
 
 $dotenv = new Dotenv(__DIR__."/../");
