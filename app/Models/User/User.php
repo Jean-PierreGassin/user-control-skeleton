ceb2482df74e8405d8355197bcd52764c467421b
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2017-02-19 23:17:39 +1000

    Swap User model to new database helpers

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -24,115 +24,82 @@ class User
 
     public function create(Request $request, $userGroup = '1')
     {
-        if ($this->exists($request->get('username'))) {
-            return;
-        }
-
         $password = password_hash($request->get('password'), PASSWORD_BCRYPT);
-        $statement = $this->adapter->connect();
-        $statement = $statement->prepare('INSERT INTO users (user, password, first_name, last_name, user_group, created_at) VALUES (?, ?, ?, ?, ?, ?)');
 
-        $statement->execute([
-            $request->get('username'),
-            $password,
-            $request->get('first_name'),
-            $request->get('last_name'),
-            $userGroup,
-            $this->timestamp
-        ]);
+        $bindings = [
+            'user' => $request->get('username'),
+            'password' => $password,
+            'first_name' => $request->get('first_name'),
+            'last_name' => $request->get('last_name'),
+            'user_group' => $userGroup,
+            'created_at' => $this->timestamp
+        ];
+
+        $this->adapter->insert('users', $bindings);
 
         return true;
     }
 
     public function update(Request $request)
     {
-        $statement = $this->adapter->connect();
-        $username = $request->get('username');
-        $password = $request->get('new_password');
-        $firstName = $request->get('first_name');
-        $lastName = $request->get('last_name');
-
         if (!password_verify($request->get('current_password'), $this->getPassword($username))) {
             return;
         }
 
-        if (!empty($password) && !empty($request->get('password_confirm'))) {
-            $password = password_hash($password, PASSWORD_BCRYPT);
-            $statement = $statement->prepare('UPDATE users SET first_name = ?, last_name = ?, password = ?, updated_at = ? WHERE user = ?');
+        if (!empty($request->get('new_password')) && !empty($request->get('password_confirm'))) {
+            $password = password_hash($request->get('new_password'), PASSWORD_BCRYPT);
 
-            $statement->execute([$firstName, $lastName, $password, $this->timestamp, $username]);
+            $bindings = [
+                'first_name' => $request->get('first_name'),
+                'last_name' => $request->get('last_name'),
+                'password' => $password,
+                'updated_at' => $this->timestamp,
+                'user' => $request->get('username')
+            ];
+
+            $this->adapter->update('users', $bindings, 'user', '=', $request->get('username'));
 
             return true;
         }
 
-        $statement = $statement->prepare('UPDATE users SET first_name = ?, last_name = ?, updated_at = ? WHERE user = ?');
+        $bindings = [
+            'first_name' => $request->get('first_name'),
+            'last_name' => $request->get('last_name'),
+            'updated_at' => $this->timestamp,
+            'user' => $request->get('username')
+        ];
 
-        $statement->execute([$firstName, $lastName, $this->timestamp, $username]);
+        $this->adapter->update('users', $bindings, 'user', '=', $request->get('username'));
 
         return true;
     }
 
-    public function exists($user)
+    public function exists(Request $request)
     {
-        $statement = $this->adapter->connect();
-        $statement = $statement->prepare('SELECT user FROM users WHERE user = ?');
-
-        $statement->execute([$user]);
-
-        while ($field = $statement->fetch(PDO::FETCH_ASSOC)) {
-            return true;
-        }
-
-        return;
+        $results = $this->adapter->select('user', 'users', 'user', '=', $request->get('username'), true);
+        
+        return (count($results) > 0);
     }
 
     public function isAdmin()
     {
-        $statement = $this->adapter->connect();
-        $statement = $statement->prepare('SELECT user_group FROM users WHERE user = ?');
-
-        $statement->execute([$this->username]);
-
-        $row = $statement->fetch(PDO::FETCH_ASSOC);
-
-        return $row;
+        return $this->adapter->select('user_group', 'users', 'user', '=', $this->username);
     }
 
     public function getInfo()
     {
-        $statement = $this->adapter->connect();
-        $statement = $statement->prepare('SELECT * FROM users WHERE user = ?');
-
-        $statement->execute([$this->username]);
-
-        return $statement->fetch(PDO::FETCH_ASSOC);;
+        return $this->adapter->select('*', 'users', 'user', '=', $this->username);
     }
 
     public function getPassword($username)
     {
-        $statement = $this->adapter->connect();
-        $statement = $statement->prepare('SELECT password FROM users WHERE user = ?');
-
-        $statement->execute([$username]);
-
-        $result = $statement->fetch(PDO::FETCH_ASSOC);
+        $result = $this->adapter->select('password', 'users', 'user', '=', $username);
 
         return $result['password'];
     }
 
     public function search($searchTerms)
     {
-        $results = [];
-        $statement = $this->adapter->connect();
-        $statement = $statement->prepare('SELECT * FROM users WHERE user LIKE ? OR first_name LIKE ? or last_name LIKE ?');
-
-        $params = array("%$searchTerms%", "%$searchTerms%", "%$searchTerms%");
-        $statement->execute($params);
-
-        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-            $results[] = $row;
-        }
-
-        return $results;
+        return $this->adapter->select('*', 'users', 'user', 'LIKE', "%$searchTerms%", true);
     }
 }
